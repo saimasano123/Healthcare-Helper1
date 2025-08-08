b511a79a07c271fa6f38e190825f697c5a6d3c4a
Cassy Cormier <93752385+prescottcassy@users.noreply.github.com>
2025-08-08 18:12:39 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "../src/App";
+import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
