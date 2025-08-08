347028b661367471e5e620af798635127ce8ebcc
Cassy Cormier <93752385+prescottcassy@users.noreply.github.com>
2025-08-08 22:09:21 +0000

    Update link to css file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import QueryForm from "./components/QueryForm";
 import ResponseCard from "./components/ResponseCard";
-import "../src/styles.css";
+import "./styles.css";
 
 function App() {
   const [response, setResponse] = useState(null);
